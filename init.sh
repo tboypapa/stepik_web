c26b42217cfce3f7b26abe486d808d506c5bf317
Kirill Kotov <tboypapag@gmail.com>
2019-09-18 22:25:54 +0300

    2.1.11 v7

--- init.sh
+++ init.sh
@@ -3,5 +3,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
-sudo gunicorn -c /etc/gunicorn.d/test-django ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
