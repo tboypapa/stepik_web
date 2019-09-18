96b41b61c3a09d09b3c07f5910a21b15c0cf6049
Kirill Kotov <tboypapag@gmail.com>
2019-09-18 22:03:28 +0300

    2.1.11 v5

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo pip3 install --upgrade django
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo gunicorn -c /etc/gunicorn.d/test-django ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
