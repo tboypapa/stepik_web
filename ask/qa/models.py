2e166db61a0380cfe307516defe0fccfd735b338
Kirill Kotov <tboypapag@gmail.com>
2019-09-20 13:32:31 +0300

    2.3.10 v1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.PositiveIntegerField(default=0)
+    author = models.ForeignKey(User, related_name="q_author", null=True, blank=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name="q_likes")
+    objects = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(User, related_name="q_answers", null=True, blank=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, related_name="a_author", null=True, blank=True, on_delete=models.SET_NULL)
--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ sudo pip3 install gunicorn==17.5
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
+~/web/ask/manage.py makemigrations qa
+~/web/ask/manage.py migrate qa
