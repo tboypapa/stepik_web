2d92fa3a09c5e352be66f04c1e0c6c81103a4b72
Kirill Kotov <tboypapag@gmail.com>
2019-09-20 14:28:49 +0300

    2.3.10 v2

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ sudo pip3 install gunicorn==17.5
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
-~/web/ask/manage.py makemigrations qa
-~/web/ask/manage.py migrate qa
+python3 ~/web/ask/manage.py makemigrations qa
+python3 ~/web/ask/manage.py migrate qa
+curl localhost:8000
